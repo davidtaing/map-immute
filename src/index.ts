b4eafa2c44c1f2078aa7b50623c6ebc4beae3a83
davidtaing <adavidtaing@gmail.com>
2023-02-25 17:04:54 +1100

    chore: add spaces to improve readability

--- src/index.ts
+++ src/index.ts
@@ -18,33 +18,41 @@ export class ImmutableMap<K, V> implements Map<K, V> {
   clear(): void {
     throw new Error("Method not implemented.");
   }
+
   delete(key: K): boolean {
     throw new Error("Method not implemented.");
   }
+
   forEach(
     callbackfn: (value: V, key: K, map: Map<K, V>) => void,
     thisArg?: any
   ): void {
     throw new Error("Method not implemented.");
   }
+
   get(key: K): V | undefined {
     return this.data.get(key);
   }
+
   has(key: K): boolean {
     throw new Error("Method not implemented.");
   }
+
   set(key: K, value: V): this {
     const mutableMap = new Map<K, V>(this);
     mutableMap.set(key, value);
 
     return new ImmutableMap<K, V>(mutableMap) as this;
   }
+
   entries(): IterableIterator<[K, V]> {
     throw new Error("Method not implemented.");
   }
+
   keys(): IterableIterator<K> {
     throw new Error("Method not implemented.");
   }
+
   values(): IterableIterator<V> {
     throw new Error("Method not implemented.");
   }
