92a5226a9d96d658562e4082675c398b55235f67
davidtaing <adavidtaing@gmail.com>
2023-02-25 12:01:53 +1100

    test: set method does not mutate original map

--- src/test/set.test.ts
+++ src/test/set.test.ts
@@ -7,3 +7,12 @@ test("set method returns a new map", () => {
 
   expect(newMap).not.toEqual(immutableMap);
 });
+
+test("set method does not mutate original map", () => {
+  const immutableMap = new ImmutableMap<string, string>();
+  immutableMap.set("a", "1");
+
+  const expected = new ImmutableMap<string, string>();
+
+  expect(immutableMap).toEqual(expected);
+});
